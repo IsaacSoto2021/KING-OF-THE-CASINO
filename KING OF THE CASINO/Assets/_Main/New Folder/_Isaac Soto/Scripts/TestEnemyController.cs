74aeaced020372a256f8ba48541d1fbf07b65442
Isaac Soto <iisoto@csuchico.edu>
2024-10-30 18:39:09 -0700

    Nav_Agent_Test_Placement
    Build the Nav Mesh and Test out AI tool in unity for better AI element and use

--- KING OF THE CASINO/Assets/_Main/New Folder/_Isaac Soto/Scripts/TestEnemyController.cs
+++ KING OF THE CASINO/Assets/_Main/New Folder/_Isaac Soto/Scripts/TestEnemyController.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
 
 public class TestEnemyController : MonoBehaviour
 {
+    public Transform Player;
+    private NavMeshAgent agent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent <NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.destination = Player.position;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            RetryGame();
+        }
+
+    }
+
+    public void RetryGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
     }
 }
+
+
+
+
