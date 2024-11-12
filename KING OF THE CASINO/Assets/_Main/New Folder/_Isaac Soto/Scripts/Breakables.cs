86f1c5d1b281cecb370fa845ea24e7b6fbca3de2
Isaac Soto <iisoto@csuchico.edu>
2024-11-12 15:06:07 -0800

    fixed problems

--- KING OF THE CASINO/Assets/_Main/New Folder/_Isaac Soto/Scripts/Breakables.cs
+++ KING OF THE CASINO/Assets/_Main/New Folder/_Isaac Soto/Scripts/Breakables.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Breakables : MonoBehaviour
 {
-    public int points = 10000;
+    public int PlayerMoney = 10000;
    
     void OnTriggerEnter(Collider other)
     {
@@ -13,33 +13,33 @@ public class Breakables : MonoBehaviour
           int Value = Random.Range(0, 5);
           if (Value == 0)
           {   
-            points -= 1000;
+                PlayerMoney -= 1000;
             Debug.Log("You lose 1000 points");
           }
           else if (Value == 1)
           {
-            points -= 2000;
+                PlayerMoney -= 2000;
             Debug.Log("You lose 2000 points");
           }
           else if (Value == 2)
           {
-            points -= 3000;
+                PlayerMoney -= 3000;
             Debug.Log("You lose 3000 points");
 
           }
           else if (Value == 3)
           {
-            points -= 4000;
+                PlayerMoney -= 4000;
             Debug.Log("You lose 4000 points");
           }
           else if (Value == 4)
-          { 
-            points -= 5000;
+          {
+                PlayerMoney -= 5000;
             Debug.Log("You lose 5000 points");
           }
           else if (Value == 5)
           {
-            points -= 6000;
+                PlayerMoney -= 6000;
             Debug.Log("You lose 6000 points");
           }
 
