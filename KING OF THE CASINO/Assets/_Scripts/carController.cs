e46ae935feb8ac39243f0e4d15a4a7d95a8cfb22
rnaustin <rnaustin@csuchico.edu>
2024-12-10 08:10:17 -0800

    Set Breakable Value

--- KING OF THE CASINO/Assets/_Scripts/carController.cs
+++ KING OF THE CASINO/Assets/_Scripts/carController.cs
@@ -75,38 +75,7 @@ public class carController : MonoBehaviour
         // Breakable collisions and randomized value chance for each collision
         if (collision.gameObject.CompareTag("Breakable"))
         {
-            int Value = Random.Range(0, 5);
-          if (Value == 0)
-            {
-                PlayerMoney -= 1000;
-                Debug.Log("You lose 1000 points");
-            }
-            else if (Value == 1)
-            {
-                PlayerMoney -= 2000;
-                Debug.Log("You lose 2000 points");
-            }
-            else if (Value == 2)
-            {
-                PlayerMoney -= 3000;
-                Debug.Log("You lose 3000 points");
-
-            }
-            else if (Value == 3)
-            {
-                PlayerMoney -= 4000;
-                Debug.Log("You lose 4000 points");
-            }
-            else if (Value == 4)
-            {
-                PlayerMoney -= 5000;
-                Debug.Log("You lose 5000 points");
-            }
-            else if (Value == 5)
-            {
-                PlayerMoney -= 6000;
-                Debug.Log("You lose 6000 points");
-            }
+            PlayerMoney -= 25000;
 
             Destroy(collision.gameObject);
         }
